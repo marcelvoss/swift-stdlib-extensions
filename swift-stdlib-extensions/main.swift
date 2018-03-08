cc10bb80971f73c446665c5f5e7663ca854f10a8
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-03-08 11:01:48 +0100

    Initial Commit

---
+++ swift-stdlib-extensions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-stdlib-extensions
+//
+//  Created by Lukas Kollmer on 08.03.18.
+//  Copyright © 2018 Lukas Kollmer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
